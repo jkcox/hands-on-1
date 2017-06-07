bd492c78f6611171d6b093e308307d08f25017f8
John Cox <thecoxcableguy@gmail.com>
2017-06-07 09:35:02 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Main method demonstrates the use of variables, operators, expressions, and statements
+            int x = 3;
+            int y = 4;
+            int sum = x + y;
+            System.Console.WriteLine("Sum of " + x + " and " + y + " is: " + sum);
+            System.Console.Write("Are the values of sum and 7 equal? " + ((sum == 7) ? true : false) +"\n");
+            string text1 = "Hello";
+            string text2 = "World";
+            string message = text1 + " " + text2;
+			System.Console.Write("Message: " + message);
+
+
+		}
+    }
+}
